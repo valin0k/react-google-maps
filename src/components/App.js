bdc11eb8d063370f0209519e828b40e71eb9b883
valin0k <serpork@ya.ru>
2018-07-14 23:19:27 +0700

    add location searchbox

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
 import { Provider as LocationProvider } from '../context/locations'
 import Map from './Map'
+import Search from './Search'
 
 class App extends Component {
   state = {
     locations: [],
   }
 
+  handleAddLocation = newLocation => {
+    this.setState(({ locations }) => ({
+      locations: locations.concat(newLocation),
+      isNewLocation: true
+    }))
+  }
+
   render() {
     const { locations } = this.state
     const providerData = {
-      locations
+      locations,
+      handleAddLocation: this.handleAddLocation,
     }
     return (
       <LocationProvider value={providerData}>
+        <Search />
         <Map />
       </LocationProvider>
     )
---
+++ src/components/Search/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import StandaloneSearchBox from 'react-google-maps/lib/components/places/StandaloneSearchBox'
+import { Consumer as MapConsumer } from '../../context/locations'
+
+class Search extends Component {
+	handlePickPlace = handler => () => {
+		const places = this.searchBox.getPlaces().slice(-1)
+		const { geometry, name } = places[0]
+		handler({
+			lat: geometry.location.lat(),
+			lng: geometry.location.lng(),
+			name
+		})
+	}
+
+	onSearchBoxMounted = ref => (this.searchBox = ref)
+
+	render() {
+		return (
+			<MapConsumer>
+				{({ handleAddLocation }) => (
+					<StandaloneSearchBox
+						ref={this.onSearchBoxMounted}
+						onPlacesChanged={this.handlePickPlace(handleAddLocation)}
+					>
+						<input type="text" placeholder="Start typing a place..." className="searchBox" />
+					</StandaloneSearchBox>
+				)}
+			</MapConsumer>
+		)
+	}
+}
+
+export default Search
