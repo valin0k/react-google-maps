1ea24ced13bdae27a6552b9a5fa6b05d4b68333c
valin0k <serpork@ya.ru>
2018-07-14 23:28:26 +0700

    show locations on the map

--- src/components/Map/GoogleMap.js
+++ src/components/Map/GoogleMap.js
@@ -1,14 +1,31 @@
 import React, { Fragment } from 'react'
-import { GoogleMap, withGoogleMap} from 'react-google-maps'
+import { GoogleMap, withGoogleMap, Marker } from 'react-google-maps'
 
 export default withGoogleMap(props => {
-	const { locationItems, isNewLocation, handleChangeCoords, openItem, handleOpenItem } = props
+	const { locationItems, openItem } = props
 	return (
 		<GoogleMap
 			defaultZoom={8}
 			center={{lat: 5, lng: 5}}
 		>
-
+      {locationItems.map((position, i) => (
+        <Fragment key={position.lng}>
+          <Marker
+            position={position}
+            label={`${++i}`}
+          />
+          {openItem &&
+          openItem.lng === position.lng &&
+          openItem.lat === position.lat && (
+            <InfoWindow
+              options={{ pixelOffset: new google.maps.Size(0, -30) }}
+              position={position}
+            >
+              <div>{position.name}</div>
+            </InfoWindow>
+          )}
+        </Fragment>
+      ))}
 		</GoogleMap>
 	)
 })
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -6,18 +6,19 @@ class Map extends Component {
 
 	render() {
 		return (
-			<div>
-				<MapConsumer>
-					{() => {
-						return (
-							<GoogleMap
-								containerElement={<div style={{ height: `400px` }} />}
-								mapElement={<div style={{ height: `100%` }} />}
-							/>
-						)
-					}}
-				</MapConsumer>
-			</div>
+      <div>
+        <MapConsumer>
+          {({ locations }) => {
+            return (
+              <GoogleMap
+                locationItems={locations}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+              />
+            )
+          }}
+        </MapConsumer>
+      </div>
 		)
 	}
 }
